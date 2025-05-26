055ef305071b77d44da8bd4d4734d8a74c5d8500
Sushil911 <Sushilbasyal30@gmail.com>
2025-05-26 16:19:22 +0545

    add companion table,recent sessions and compa nion cta

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,51 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-lg underline'>
-        Hello there
+    <main>
+      <h1>
+        Popular Companions
       </h1>
-      <Button>
-        Let's get started
-      </Button>
-    </div>
+      <section className='home-section'>
+        <CompanionCard
+        id='123'
+        name='Neura the Brainy Explorer'
+        topic='Neural Network of the Brain'
+        duration={45}
+        subject='Science'
+        color='#ffda6e'
+        />
+        <CompanionCard
+        id='456'
+        name='Countsy the Number Wizard'
+        topic='Derivatives and Integrals'
+        duration={30}
+        subject='maths'
+        color='#e5d0ff'
+        />
+        <CompanionCard
+        id='789'
+        name='Verba the Vocabulary Builder'
+        topic='English Literature'
+        duration={30}
+        subject='language'
+        color='#dbe7ff'
+        />
+      </section>
+      <section className='home-section'>
+        <CompanionsList
+        title='Recently completed session'
+        companions={recentSessions}
+        classNames='w-2/3 max-lg:w-full'
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
