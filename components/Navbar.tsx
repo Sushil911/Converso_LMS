dfccd57e02188d4e0606f39f73bb8fa0754aa83e
Sushil911 <Sushilbasyal30@gmail.com>
2025-05-29 14:13:46 +0545

    add sign-in with clerk

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,12 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import NavItems from './NavItems'
+import { SignInButton,SignedIn,SignedOut,UserButton, useAuth } from '@clerk/nextjs'
 
 const Navbar = () => {
+    
   return (
     <nav className='navbar'>
         <Link href='/'>
@@ -18,7 +21,16 @@ const Navbar = () => {
         </Link>
         <div className='flex items-center gap-8'>
             <NavItems />
-            <p>Sign In</p>
+            <SignedOut>
+                    <SignInButton>
+                        <button className='btn-signin'>
+                            Sign In
+                        </button>
+                    </SignInButton> 
+            </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
         </div>
     </nav>
   )
